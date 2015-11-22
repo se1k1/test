204d150ec0c414415c38c37bebf932e006883f7b
Kae Sawada <se1k1h1mawar1@hotmail.com>
2015-11-22 15:37:32 -0800

    sequentialSearchMSD() draft 1.5

---
+++ ReferenceFrameBlock.java
@@ -0,0 +1,54 @@
+public class ReferenceFrameBlock {
+	private int xTopLeft = 0, yTopLeft = 0;
+	private float diffValue = 0;
+
+	public ReferenceFrameBlock()
+	{
+	}
+
+	public ReferenceFrameBlock( int x, int y, float diffVal )
+	{
+		xTopLeft = x;
+		yTopLeft = y;
+		diffValue = diffVal;
+	}
+
+	public int getxTopLeft()
+	{
+		return xTopLeft;
+	}
+
+	public void setxTopLeft( int xTopLeft )
+	{
+		this.xTopLeft = xTopLeft;
+	}
+
+	public int getyTopLeft()
+	{
+		return yTopLeft;
+	}
+
+	public void setyTopLeft( int yTopLeft )
+	{
+		this.yTopLeft = yTopLeft;
+	}
+
+	public float getDiffValue()
+	{
+		return diffValue;
+	}
+
+	public void setDiffValue( float diffValue )
+	{
+		this.diffValue = diffValue;
+	}
+
+	@Override
+	public String toString()
+	{
+		return "Re<" + xTopLeft + ", "
+				+ yTopLeft + "> diffValue=" + diffValue;
+	}
+	
+
+}
