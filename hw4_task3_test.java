3c039509ac817a5701a774983e6c56bd28f0f89a
Kae Sawada <se1k1h1mawar1@hotmail.com>
2015-11-30 12:09:27 -0800

    before erasing Similarity.java from package Similarity

--- hw4_task3_test.java
+++ hw4_task3_test.java
@@ -4,22 +4,32 @@ import java.util.Random;
 
 import org.junit.Test;
 
-
 public class hw4_task3_test {
 
 	@Test
+	public void test_getTop3()
+	{
+		Task3 t3 = new Task3();
+		Prep pp = new Prep();
+		int targetNum = 59;
+
+		Similarity[] sims = new Similarity[3];
+		sims = t3.getTop3SimilarFrames( 4, 16, targetNum );
+	}
+
+	// @Test
 	public void test_deepcopy2dArray()
 	{
 		Task3 t3 = new Task3();
 		Prep pp = new Prep();
-		
-		int[][] a= new int[randomNumGen( 5, 10 )][randomNumGen( 2, 4 )];
+
+		int[][] a = new int[randomNumGen( 5, 10 )][randomNumGen( 2, 4 )];
 		autoFill2dArray( a, 0, 15 );
 		int[][] b = t3.deepCopy2dArray( a );
-		
-		System.out.println("a:");
+
+		System.out.println( "a:" );
 		pp.print2DArray( a );
-		System.out.println("b:");
+		System.out.println( "b:" );
 		pp.print2DArray( b );
 	}
 
@@ -27,12 +37,12 @@ public class hw4_task3_test {
 	{
 		for ( int i = 0; i < array.length; i++ ) {
 			for ( int j = 0; j < array[0].length; j++ ) {
-						array[i][j] = randomNumGen( min, max );
-								
+				array[i][j] = randomNumGen( min, max );
+
 			}
 		}
 	}
-	
+
 	public void autoFill3dArray( int[][][] array, int minDiff, int maxDiff,
 			int minMV, int maxMV )
 	{
